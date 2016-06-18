af3abcfbc9757fef9b1ab71a4244f0d9b39918a8
Nick Martinez <23nicholasmartinez@gmail.com>
2016-06-17 20:34:13 -0700

    Create webmail_client.js

---
+++ webmail_client.js
@@ -0,0 +1,26 @@
+function WebmailViewModel() {
+    // Data
+    var self = this;
+    self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
+    self.chosenFolderId = ko.observable();
+    self.chosenFolderData = ko.observable();
+    self.chosenMailData = ko.observable();
+    
+    // Behaviors
+    self.goToFolder = function(folder) {
+        self.chosenFolderId(folder);
+        self.chosenMailData(null);
+        $.get('/mail', { folder: folder }, self.chosenFolderData);
+    };
+    
+    self.goToMail = function(mail) {
+        self.chosenFolderId(mail.folder);
+        self.chosenFolderData(null);  // Stop showing folder
+        $.get("/mail", { mailId: mail.id }, self.chosenMailData);
+    };
+    
+    // Show inbox by default
+    self.goToFolder('Inbox');
+};
+
+ko.applyBindings(new WebmailViewModel());
