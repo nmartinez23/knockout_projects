5524e76c2c24f71d4e95245c8f1db4e8bb82f8e9
Nick Martinez <23nicholasmartinez@gmail.com>
2016-06-17 19:28:16 -0700

    Create name_input.js

---
+++ name_input.js
@@ -0,0 +1,18 @@
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+
+function AppViewModel() {
+    this.firstName = ko.observable("Nick");
+    this.lastName = ko.observable("Martinez");
+    
+    this.fullName = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();
+    }, this);
+    
+    this.capitalizeLastName = function() {
+        var currentVal = this.lastName();
+        this.lastName(currentVal.toUpperCase());
+    };
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
